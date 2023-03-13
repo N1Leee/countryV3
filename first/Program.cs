16ebf4b78d4a474bfb85d1b529350c19a329bb2a
Nikita Malaxov <rudseknikita30@gmail.com>
2023-03-13 09:55:04 +0400

    firstCommit

---
+++ first/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Country
+    {
+        public string Name { get; set; }
+        public string FormOfGovernment { get; set; }
+        public float Square { get; set; }
+    }
+}
